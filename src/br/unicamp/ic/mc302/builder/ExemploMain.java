e27a0155989f7eae1843e15d529d7a189469ba78
João Scalett <joao.scalett@gmail.com>
2016-04-16 18:15:19 -0300

    Invertendo ordem de exemplos de construção do builder
    Signed-off-by: João Scalett <joao.scalett@gmail.com>

--- src/br/unicamp/ic/mc302/builder/ExemploMain.java
+++ src/br/unicamp/ic/mc302/builder/ExemploMain.java
@@ -5,17 +5,20 @@ import java.awt.Point;
 
 public class ExemploMain {
 	public static void main(String args[]) {
-		StreetMap map = new StreetMap.Builder(new Point(50, 50),
-				new Point(100, 100)).landColor(Color.GRAY)
-						.waterColor(Color.BLUE.brighter()).build();
-		System.out.println(map);
-
-		// Reescrevendo passo a passo:
+		StreetMap map;
+		// construindo passo a passo:
 		StreetMap.Builder builder = new StreetMap.Builder(new Point(50, 50),
 				new Point(100, 100));
 		builder.landColor(Color.GRAY);
 		builder.waterColor(Color.BLUE.brighter());
 		map = builder.build();
+		System.out.println(map);
+
+		System.out.println("\n\n\n");
+		// reescrevendo o código em apenas uma linha
+		map = new StreetMap.Builder(new Point(50, 50), new Point(100, 100))
+				.landColor(Color.GRAY).waterColor(Color.BLUE.brighter())
+				.build();
 		System.out.println(map);
 	}
 }
